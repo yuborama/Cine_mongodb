a71e18e9991ddbe9a3102e7aafe0e4fae9097891
Yuborama <davidjohan2@hotmail.com>
2020-12-15 11:03:21 -0500

    fix app

--- src/server.js
+++ src/server.js
@@ -2,8 +2,10 @@ const express = require('express');
 
 const app = express();
 
-
-
+app.set('port', process.env.PORT || 4001);
 
 
 app.use(require('./routes/movie.routes'));
+
+
+module.exports = app;
